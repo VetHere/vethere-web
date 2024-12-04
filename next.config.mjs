6ff30e32a9c5d305039d97115d116c2c8cee9691
danieliank <daxter1835@gmail.com>
2024-12-04 11:31:54 +0700

    docs: change config for cors issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Match all API requests starting with /api/
+        destination: "http://localhost:8000/:path*", // Proxy to backend
+      },
+    ];
+  },
+};
 
 export default nextConfig;
