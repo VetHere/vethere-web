fc78ec372cf2a8290b95bf97212f5b47bbe8f3c0
danieliank <daxter1835@gmail.com>
2024-12-12 22:30:06 +0700

    feat: add login access token

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  console.log(req.nextUrl);
+  const res = NextResponse.next();
+  req.headers.append("ACCESS-CONTROL-ALLOW-ORIGIN", "*");
+  return res;
+}
+
+export const config = {
+  matcher: ['/api/":path*'],
+};
